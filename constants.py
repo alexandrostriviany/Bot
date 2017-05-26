cd747a6a2e0596321cd8da0df1d06213addf5427
Alexandr <alexandrostriviany@gmail.com>
2017-05-26 18:43:10 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-token = "356162982:AAG9VA0e_M-YqgfL6Mcm6caH3ao89H6MDJo"
+token = "272606949:AAHp1YBrjVlabEtgRffF7eNa9CFZfCFTvzk"
 
 start_message = "Приветики, я помогу вам отморозиться от любого движа в стиле Алины.\nПросто введите /alinahelpme"
 help_message = "Бог в помощь, а мне змейку проходить нужно.."
