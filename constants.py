edd59b0121af27064d1da1c2114693afbcfe648d
Alexandr <alexandrostriviany@gmail.com>
2017-05-26 18:54:15 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -4,4 +4,4 @@ token = "272606949:AAHp1YBrjVlabEtgRffF7eNa9CFZfCFTvzk"
 
 start_message = "Приветики, я помогу вам отморозиться от любого движа в стиле Алины.\nПросто введите /alinahelpme"
 help_message = "Бог в помощь, а мне змейку проходить нужно.."
-webhook = "https://alinasexcusesbot.herokuapp.com"
+webhook = "https://alinabot.herokuapp.com/"
