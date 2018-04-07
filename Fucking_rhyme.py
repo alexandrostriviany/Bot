173804e267171dbf3e770eb271597a39667e8768
alexandrostriviany <alexandrostiviany@gmail.com>
2018-04-07 18:31:38 +0300

    fix fucking rhyme

--- Fucking_rhyme.py
+++ Fucking_rhyme.py
@@ -8,16 +8,17 @@ rhymes_dictionary = {
     "да": ["пизда"],
     "триста": ["отсоси у тракториста"],
     "смысле": ["в коромысле"],
-    "че": ["хер через плечё"],
+    "че": ["хер через плечё", "капче"],
     "а": ["хуй на"],
     "шо": ["капшо"],
+    "где": ["в пизде"]
 }
 
 
 def send_rhyme(message):
-    word = str.split(re.sub(r'[\W]', ' ', message.text).strip(), ' ')
+    word = str.split(re.sub(r'[\W]', ' ', message).strip(), ' ')
     word.reverse()
     if len(word) > 0:
-        word = word[0]
-        if word.lower() in rhymes_dictionary:
+        word = word[0].lower()
+        if word in rhymes_dictionary:
             return random.choice(rhymes_dictionary[word])
